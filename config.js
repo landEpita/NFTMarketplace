167bd12070551bdf88e843e188aea270891d9322
thomas.gossin <thomas.gossin@epita.fr>
2022-02-17 16:55:59 +0100

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+// export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+// export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const nftaddress = "0x175665042EC720C89dE1ad96eA0f2329D55AB21D";
+export const nftmarketaddress = "0x17028b3a1B5BeF6D3b113a5A81DAD13f16156D03";
---
+++ hardhat.config.js
@@ -0,0 +1,28 @@
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const privateKey =
+  fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+
+const projectId = "46885e83313240119f54bfd810ce0439";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
